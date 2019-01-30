c6cddb2eda62954176448030aa5909939e70b3b5
AlejandrinaJimenez <fairuza12@gmail.com>
2019-01-30 15:45:24 -0400

    Archivo de definicion de variables

---
+++ avances/1_variable_definition.py
@@ -0,0 +1,13 @@
+"""
+Los comentarios en Python comienzan con el caracter (#) e incluye 
+Agregue un nuevo comentario al archivo.
+"""
+
+a=b=2 #Esto se llama una "asignacion encadenada"
+print("a = "+ str(a))
+print("b = " + str(b))
+
+saludos = "saludos" #greetings
+print("saludos = " + saludos)
+saludos = 3
+print("saludos = " + str(saludos))
