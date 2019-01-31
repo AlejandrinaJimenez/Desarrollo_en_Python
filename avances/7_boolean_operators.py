020bac7167807353a64f987b2974dcbde5b010c6
AlejandrinaJimenez <fairuza12@gmail.com>
2019-01-31 15:51:16 -0400

    operadores booleanos

---
+++ avances/7_boolean_operators.py
@@ -0,0 +1,20 @@
+two = 2
+three = 3
+is_equal = two == three
+print (is_equal)
+
+name = "John"
+age=17
+print(name=="John" or age ==17)
+
+print(name == "John" and not (age == 23))
+
+name = "John"
+age=17
+
+print(name=="John" or not age>17)
+
+print(name is "Ellis" or not (name is "John" and age == 17))
+
+x= 1+2**3/4*5
+print(x)
