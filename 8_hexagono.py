a0dc948666a1cc315bf06283abf14e3f1553d50c
Alejandrina Jiménez Guzmán <fairuza12@gmail.com>
2019-02-14 17:41:47 -0400

    Update 8_hexagono.py

--- 8_hexagono.py
+++ 8_hexagono.py
@@ -1,9 +1,10 @@
 
 def hexagono(n):
     for i in range(n):
-        print (' '*(n-i-1)+'*' *(i+n))
+        print (' '*(n-i-1)+'*' *(2*i+n))
     for j in range(n-1,0,-1):
-        print(' '*(n-j)+'*' *(j+n-1))
+        print(' '*(n-j)+'*' *(2*j+n-1))
+    
 
 x = int(input("Ingrese la longitud del lado del hexagono:"))
 y = hexagono(x)
